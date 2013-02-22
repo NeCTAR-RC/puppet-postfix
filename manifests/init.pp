d5e32a7a2796b63cb052944e4ca1b5222aa6ace7
Sam Morrison <sorrison@gmail.com>
2013-02-22 14:50:05 +1100

    Split classes to allow for mail server
    Change-Id: Ia105d4b851523edfc7871f1a60e315242cfacadb

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,26 @@
 class postfix {
 
-  package {'postfix':
+  package { ['postfix', 'bsd-mailx']:
     ensure => installed,
   }
 
-  service {'postfix':
+  service { 'postfix':
     ensure => running,
     require => Package['postfix'],
   }
 
-  file {'/etc/postfix/main.cf':
+  file { '/etc/mailname':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => $::fqdn,
+  }
+}
+
+class postfix::client inherits postfix {
+
+  file { '/etc/postfix/main.cf':
     ensure  => present,
     owner   => root,
     group   => root,
@@ -19,5 +30,18 @@ class postfix {
     require => Package['postfix'],
   }
 
-  
+}
+
+class postfix::server inherits postfix {
+
+  file { '/etc/postfix/main.cf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('postfix/server.cf.erb'),
+    notify  => Service['postfix'],
+    require => Package['postfix'],
+  }
+
 }
