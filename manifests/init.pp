a1b126b500d7a5bcdd94b9a9fef2c6bafb6ace41
Marcus Furlong <furlongm@gmail.com>
2013-03-13 14:53:56 +1100

    add standard aliases file and run newaliases
    Change-Id: I2ea3dba1cc99da0c9f86b3bb749bd5cd309969f5

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,14 @@
-class postfix($smtp_host, $inet_interfaces='loopback-only') {
+class postfix($smtp_host, $root_email_address, $inet_interfaces='loopback-only') {
 
-  package { ['postfix', 'bsd-mailx']:
-    ensure => installed,
+  $postfix_pkgs = ['postfix', 'bsd-mailx']
+
+  package { $postfix_pkgs :
+    ensure  => installed,
   }
 
   service { 'postfix':
     ensure => running,
-    require => Package['postfix'],
+    require => Package[$postfix_pkgs],
   }
 
   file { '/etc/mailname':
@@ -24,7 +26,26 @@ class postfix($smtp_host, $inet_interfaces='loopback-only') {
     mode    => '0644',
     content => template('postfix/main.cf.erb'),
     notify  => Service['postfix'],
-    require => Package['postfix'],
+    require => Package[$postfix_pkgs],
   }
 
+  file { '/etc/aliases':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('postfix/aliases.erb'),
+    notify  => [Service['postfix'], Exec['newaliases']],
+    require => Package[$postfix_pkgs],
+  }
+
+  exec { 'newaliases':
+    command     => '/usr/bin/newaliases',
+    refreshonly => true,
+  }
+
+  # legcay way
+  file {'/root/.forward':
+    ensure  => absent,
+  }
 }
